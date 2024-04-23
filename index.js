3da9ac0a16e7b0ded4ba49880bc3f7e193261a6c
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-04-23 13:44:12 +0500

    t

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require("express");
+const express = require('express')
 const mongoose = require('mongoose')
 const postRouter = require('./routes/postRouter.js')
 const fileUpload = require('express-fileupload')
 require('dotenv').config()
 
-
-const app = express();
+const app = express()
 app.use(express.json())
 app.use(fileUpload({}))
 app.use('/api', postRouter)
 app.use(express.static('static'))
 
-app.get('/', (req,res)=>{
-  res.redirect('api/posts')
+app.get('/', (req, res) => {
+	res.redirect('api/posts')
 })
 
-
-
 const startApp = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true, useUnifiedTopology:true})
-    app.listen(process.env.PORT, () => console.log("server running"));
-  } catch (error) {
-    console.log(error);
-  }
-};
+	try {
+		await mongoose.connect(
+			'mongodb+srv://crud:crud@crud-cluster.9bduvrt.mongodb.net/?retryWrites=true&w=majority&appName=crud-cluster',
+			{ useNewUrlParser: true, useUnifiedTopology: true }
+		)
+		app.listen(5000, () => console.log('server running'))
+	} catch (error) {
+		console.log(error)
+	}
+}
 startApp()
