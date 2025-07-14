46ca4fddc884b42a334ea48ad4ffaef25851c7ec
Alimov Kamron <kamronalimov80@gmail.com>
2025-07-14 16:06:02 +0500

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -5,7 +5,7 @@ const Post  = new Schema({
     author:{type:String,required: true},
     title:{type:String,required:true},
     content:{type:String,required:true},
-    picture:{type:String}
+    picture:{type:String,required:false}
 })
 
 module.exports = model('Post',Post)
